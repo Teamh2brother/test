2bc385e5647566342994af094c43d0f81435080e
Teamh2brother <quangthanghocmai3@gmail.com>
2021-12-03 22:54:38 +0700

    test

--- Web_TLIT/js/script.js
+++ Web_TLIT/js/script.js
@@ -8,3 +8,6 @@ $('.slider').slick({
    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='btn__ btn__left fa fa-angle-left' aria-hidden='true'></i></button>",
    nextArrow:"<button type='button' class='slick-next pull-right'><i class='btn__ btn__right fa fa-angle-right' aria-hidden='true'></i></button>"
 })
+function gotogioithieu(){
+   location.href = '../../GIOITHIEU_TLIT/index.html';
+}
